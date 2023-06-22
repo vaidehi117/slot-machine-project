5986d717f5b9c681d1b77926118fedaf1d04ef70
vaidehi <vaidehi117v@gmail.com>
2023-06-22 11:38:57 -0400

    organized

--- app.js
+++ app.js
@@ -1,160 +1,91 @@
-  /*----- constants -----*/
-  const item1 = document.getElementById('item1');
-  const item2 = document.getElementById('item2');
-  const item3 = document.getElementById('item3');
+var symbols = ['🍒', '🍋', '🍊', '🍇', '🔔', '💎'];
+var slot1, slot2, slot3;
+var credits = 100; // Initial credits
 
-  /*----- state variables -----*/
-let credits = 25;
-let totalBet = 0;
-let betAmount = 100; //initial bet amount 
+// Add event listener to the "Spin" button
+document.getElementById('spinButton').addEventListener('click', spin);
+// Add event listener to the "Cashout" button
+document.getElementById('cashoutButton').addEventListener('click', cashout);
 
-  /*----- cached elements  -----*/
-  const input = document.querySelector("input");
-  const creditsElement = document.getElementById('credits');
+function spin() {
+  var betAmount = parseInt(document.getElementById('betInput').value);
 
-
-  /*----- event listeners -----*/
-  document.getElementById('spin-button').addEventListener('onClick', function() {
-    spin();
-  });
-
-  document.getElementById('increase-bet').addEventListener('click', function() {
-    increaseBet();
-  });
-
-  document.getElementById('cash-out').addEventListener('click', function() {
-    cashOut();
-  });
-  
-
-
-  /*----- functions -----*/
-  const symbols = ['🍒', '🍋', '🍊', '🍇', '🔔', '💎'];
-
-
-  function spin() {
-    const symbols = ['🍒', '🍋', '🍊', '🍇', '🔔', '💎'];
-    const randomIndex = Math.floor(Math.random() * symbols.length);
-    return symbols[randomIndex];
+  // Check if the bet amount is valid
+  if (betAmount < 10 || betAmount > 100) {
+    renderResult('Invalid bet amount. Please enter a value between $10 and $100.', 'error', document.getElementById('result'));
+    return;
   }
+  // Deduct bet amount from credits
+  credits -= betAmount;
 
-  function slotSpin() {
 
-    const items = document.getElementsByClassName('item');
+  // Disable the button during spinning to prevent multiple spins
+  document.getElementById('spinButton').disabled = true;
 
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i];
-      item.innerHTML = ''; // Clear the current symbols
-  
-      for (let j = 0; j < 3; j++) {
-        const symbol = document.createElement('span');
-        symbol.classList.add('slot-symbol');
-        symbol.textContent = getRandomSymbol();
-        reel.appendChild(symbol);
-      }
-    }
+  //Generate random symbols for each slot 
+  slot1 = Math.floor(Math.random() * symbols.length);
+  slot2 = Math.floor(Math.random() * symbols.length);
+  slot3 = Math.floor(Math.random() * symbols.length);
 
-    if (credits >= 5) {
-      credits -= 5; // Deduct 5 credits for each spin
-      render();
-      updateCredits();
-      updateBetAmount();
-    } else {
-      alert('Insufficient credits!');
-    }
+   // Start spinning animation
+   animateSpin();
 
-    if (credits >= betAmount) {
-      // Deduct bet amount from credits
-      credits -= betAmount; 
-      //Increase the total bet amount
-      totalBet += betAmount;
-      render();
-      updateCredits();
-      updateTotalBet();
-    } else {
-      alert('Insufficient credits!');
-    }
-
-    //show message if winner condition 
-    if (num1 == num2 && num1 == num3) {
-      showMessage();
-    } else {
-      hideMessage();
-    }  
-  }
-
-  //calculate bet amount
-  function increaseBet() {
-    if (betAmount < credits) {
-      betAmount += 10; // Increase bet amount by 10
-      updateBetAmount();
-    } else {
-      alert('Insufficient credits for increasing the bet!');
-    }
-  }
-
-  function render() {
-    item1.textContent = spin();
-    item2.textContent = spin();
-    item3.textContent = spin();
-  }
-
-//update totalBet 
-function updateTotalBet() {
-  const totalBetElement = document.getElementById('total-bet');
-  totalBetElement.textContent = totalBet;
+   // After a delay, stop the spinning animation and update the UI
+   setTimeout(function() {
+     stopAnimateSpin();
+     updateUI();
+     checkWin();
+     // Enable the button after the spinning is done
+     document.getElementById('spinButton').disabled = false;
+   }, 2000); // Adjust the delay (in milliseconds) to control the spinning duration
 }
 
-//Update Bet amount
-function updateBetAmount() {
-  const betAmountElement = document.getElementById('bet-amount');
-  betAmountElement.textContent = betAmount;
+function animateSpin() {
+  // Implement your spinning animation logic here
+  // This function will be called when the spin button is clicked
+  // and before updating the UI with the final symbols
 }
-updateBetAmount();
 
-//Updates cradits 
-function updateCredits() {
-  creditsElement.textContent = credits;
+function stopAnimateSpin() {
+  // Implement your logic to stop the spinning animation here
+  // This function will be called after the delay
 }
-document.getElementById('spin-button').addEventListener('click', function() {
-  if (credits >= 5) {
-    slotSpin();
+
+function slotUpdate() {
+  document.getElementById('slot1').innerHTML = symbols[slot1];
+  document.getElementById('slot2').innerHTML = symbols[slot2];
+  document.getElementById('slot3').innerHTML = symbols[slot3];
+  document.getElementById('credits').innerHTML = credits;
+}
+
+//Checks is the user is winner 
+function checkWinner() {
+  var resultContainer = document.getElementById('result');
+  if(slot1 == slot2 && slot2 == slot3) {
+    render('!!You Won!!', 'win', resultContainer);
   } else {
-    alert('Insufficient credits!');
-  }
-});
-
-//check win 
-function checkWin() {
-  if (item11 === item2 && item2 === item3) {
-    alert('Congratulations! You won!');
-  } else {
-    alert('Sorry, try again.');
+    render('Try Again.', 'lose', resultContainer);
   }
 }
 
-// Display the message is the user wins 
-function showMessage() {
-  const message = document.getElementById("message");
-  message.style.display = 'block';
-  message.classList.add('animated', 'pulse');
+function calculateWinAmount(betAmount) {
+  // Customize the win amount calculation logic as per your game rules
+  // For example, you can use multipliers or fixed win amounts based on the winning symbol combination
+  return betAmount * 5; // Multiplier of 5 for this example
 }
 
-function hideMessage() {
-  const message = document.getElementById("message");
-  message.style.display = "none";
+// Reset the slot machine state
+function cashout() {
+  slot1 = null;
+  slot2 = null;
+  slot3 = null;
+  credits = 100; // Reset credits to the initial value
+
+  slotUpdate();
+  // Clear the result message
+  render('', '', document.getElementById('result')); 
 }
 
-//Chashout 
-function cashOut() {
-  alert(`You cashed out ${cradits}.`)
-  //Reset the cradits to zero
-  credits = 0;
-  totalBet = 0;
-  updateTotalBet();
-  updateCredits();
+function render(message, resultClass,container) {
+  container.innerHTML = '<p class="' + resultClass + '">' + message + '</p>';
 }
-
-updateCredits();
-updateBetAmount();
-updateTotalBet();
